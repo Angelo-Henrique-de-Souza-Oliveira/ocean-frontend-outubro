031921e2bf7cf9aea75c5024cd2c08eac0a6589a
Angelo-Henrique-de-Souza-Oliveira <106783757+Angelo-Henrique-de-Souza-Oliveira@users.noreply.github.com>
2023-11-14 23:50:41 +0000

    carregardadosapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from 'react'
 import './App.css'
 import Card from './components/Card/Card'
+import { useEffect } from 'react'
 function App() {
   const item1 = {
     name: "Rick Sanchez",
@@ -9,20 +11,40 @@ function App() {
   }
   const item2 = {
     name: "Morty Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
-    tags:[]
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+    tags:["Status: vivo","Espécie:Humana","Origem: Terra C-137"]
   }
   const item3 = {
     name: "Summer Smith",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
-    tags:[]
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg",
+    tags:["Status: vivo","Espécie:Humana","Origem: Terra C-137"]
   }
-  const items = [item1,item2,item3]
+ // const items = [item1,item2,item3]
+
+  const [items, setItems]= useState([])
+
+  async function carregarDadosapi(){
+    
+  const apiurl = "https://rickandmortyapi.com/api/character/";
+
+  const response = await fetch(apiurl)
+
+  const body = await response.json()
+  console.log(body);
+
+  setItems(body.results)
+  }
+  useEffect(function (){
+    carregarDadosapi()
+  }, [])
+
   return (
     <>
+    <div className='cards'>
     {items.map(function(element){
       return <Card item={element}/>
     })}
+    </div>
       </>
   )
 }
