05f0ff15042716ae077a969c7d6a1766d05f4f94
Angelo-Henrique-de-Souza-Oliveira <106783757+Angelo-Henrique-de-Souza-Oliveira@users.noreply.github.com>
2023-11-14 23:07:55 +0000

    tagsmaps

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,19 @@ import Card from './components/Card/Card'
 function App() {
   const item1 = {
     name: "Rick Sanchez",
-    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    tags:["Status: vivo","Espécie:Humana","Origem: Terra C-137"]
+
   }
   const item2 = {
     name: "Morty Smith",
     imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    tags:[]
   }
   const item3 = {
     name: "Summer Smith",
     imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    tags:[]
   }
   const items = [item1,item2,item3]
   return (
